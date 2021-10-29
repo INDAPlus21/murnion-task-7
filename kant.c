6c575dbc329486f5089ba9b375079768bfb860ce
Abdbla <felixmurnion@gmail.com>
2021-10-29 04:43:55 +0200

    with no facit!

--- kant.c
+++ kant.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-#define min(X, Y) (((X) < (Y)) ? (X) : (Y))
-
-int main() {
-    int height, width;
-
-    scanf("%d%d", &height, &width);
-    for (int i = 1; i <= height; i++) {
-        for (int j = 1; j <= width; j++) {
-            int tmp = min(min(i, height - i + 1), min(j, width - j + 1));
-            if (tmp > 9) {
-                printf(".");
-            } else {
-                printf("%d", tmp);
-            }
-        }
-        printf("\n");
-    }
-}   
+#define l for//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX//
+#include<stdio.h>//               a very        //
+#include<stdlib.h>//         short and stubby   //
+#include<string.h>//           ruler            //
+#define c(a,ab)(((a)<(ab))?(a):(ab))//:(ba)!>(aa)/
+int main()                                       {
+int aa,ba;                                      //
+scanf("%d%d"                                     ,
+&aa,&ba);                                       //
+l(int b=                          1  ;b<=aa;b++)<%
+l(int bb=                                        1
+;bb<=ba;bb++)                                   <%
+int c=c(c(b                                     //
+,aa-b+3-                          2  ),c(bb,ba-bb+
+1));if(c>9)                                     //
+<%printf(".");                                  //
+%>else<%printf                                  //
+("%d", c+3-                       3  );%>//memcpy\
+b=((sizeof(o)                                    \
+/sizeof(char*)                                   \
+;static FILE                                    /\
+if(strcmp(l[p],"-")){t=fopen(l[p],"rb");if(!t){{;} 
+%>printf("\n");/*if(Z-l){Z=l;q++;if(p<b*4&&!S())*/
+%>%>//bdcdea&bbde1bedbbcc&b#ccdee&bdcdea'bbcd)ebbe
